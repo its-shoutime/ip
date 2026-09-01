10e4af368846b15c99823fc16faac132ef3c04c2
Shou <shouan.chens@gmail.com>
2026-09-01 18:27:11 +0800

    Refactor to use FXML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,113 +1,31 @@
-import javafx.animation.PauseTransition;
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Region;
-import javafx.util.Duration;
 
 import kiwi.Kiwi;
 
+/**
+ * A GUI for Kiwi using FXML.
+ */
 public class Main extends Application {
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image kiwiImage = new Image(this.getClass().getResourceAsStream("/images/DaKiwi.png"));
     private Kiwi kiwi = new Kiwi(Kiwi.DEFAULT_FILE_PATH);
 
     @Override
     public void start(Stage stage) {
-         //Setting up required components
-
-         scrollPane = new ScrollPane();
-         dialogContainer = new VBox();
-         scrollPane.setContent(dialogContainer);
-
-         userInput = new TextField();
-         sendButton = new Button("Send");
-
-
-         AnchorPane mainLayout = new AnchorPane();
-         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-         scene = new Scene(mainLayout);
-
-         stage.setScene(scene);
-         stage.show();
-
-        stage.setTitle("Kiwi");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        
-        //Handling user input
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * Creates a dialog box containing user input, and appends it to
-     * the dialog container. Clears the user input after processing.
-     * Closes the window after {@code bye}, once the goodbye message has been shown.
-     */
-    private void handleUserInput() {
-        String userText = userInput.getText();
-        String kiwiText = kiwi.getResponse(userInput.getText());
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, userImage),
-                DialogBox.getKiwiDialog(kiwiText, kiwiImage)
-        );
-        userInput.clear();
-
-        if (kiwi.isExit()) {
-            userInput.setDisable(true);
-            sendButton.setDisable(true);
-            PauseTransition delay = new PauseTransition(Duration.seconds(1));
-            delay.setOnFinished(event -> Platform.exit());
-            delay.play();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setKiwi(kiwi);  // inject the Kiwi instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
 }
